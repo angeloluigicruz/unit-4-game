70da042e74f17f68464346b5c96fd7f538dcf25a
Luigi Cruz <angeloluigicruz@gmail.com>
2019-07-13 20:02:47 -0700

    javascript needs work

---
+++ assets/javascript/game.js
@@ -0,0 +1,5 @@
+// <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+var randomNumber = Math.floor(Math.random() * 120) + 19;
+
+$(".random-number").text(randomNumber);
