eb66583656c2018686afc12cfd1a5fbb800ac25a
Luigi Cruz <angeloluigicruz@gmail.com>
2019-07-20 11:04:18 -0700

    updates to solve total score

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,1 @@
 // <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-
-var randomNumber = Math.floor(Math.random() * 120) + 19;
-
-$(".random-number").text(randomNumber);
